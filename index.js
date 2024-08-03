8f6382b736733a3ee8a2bd6a5e638626c9236ef6
Aniket Das <81286242+aniketdas2002@users.noreply.github.com>
2024-08-03 12:49:23 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontains = document.getElementsByClassName("tab-contains");
+
+function opentab(tabname) {
+  for (let tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (let tabcontain of tabcontains) {
+    tabcontain.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
